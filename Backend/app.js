3b8c62516cc1722393cdc3177dbcbc069ad961ff
Vishwamitra Kumar Singh <kumarvishwamitra14@gmail.com>
2025-08-21 22:24:18 +0530

    My Portfolio

--- Backend/app.js
+++ Backend/app.js
@@ -2,25 +2,29 @@ const express = require("express");
 const mongoose = require("mongoose");
 const userRouter = require("./routes/userRoute.js");
 const cors = require("cors");
-const app = express();
 const dotenv = require("dotenv");
+
 dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 
-// Middlewares
-app.use(cors()); 
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
+
 
 app.use("/user", userRouter);
 
 const MONGO_URI = process.env.MONGO_URI;
 async function main() {
   try {
-    await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(MONGO_URI);
     console.log("✅ Connected to DB");
   } catch (err) {
     console.error("❌ DB connection error:", err);
@@ -28,7 +32,7 @@ async function main() {
 }
 main();
 
-// Start server
-app.listen(3000, () => {
-  console.log("🚀 Server is running on port 3000");
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
 });
